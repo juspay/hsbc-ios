2f4e9dbac4deba1603dcbaaecf1649c94e64e2ec
Namit Goel <namit.goel@juspay.in>
2025-08-18 16:06:57 +0530

    feat: initiat commit

---
+++ Sources/HSBCPaymentsSDK/HSBCPaymentsServices.m
@@ -0,0 +1,86 @@
+//
+//  HSBCPaymentsServices.m
+//
+
+#import "HSBCPaymentsServices.h"
+#import "HSBCPaymentsUtils.h"
+#import "HSBCPaymentsConstants.h"
+#import "HSBCPaymentsTenantMap.h"
+
+@interface HSBCPaymentsServices()
+
+@property (nonatomic, strong) NSString *tenantServiceName;
+
+@end
+
+@implementation HSBCPaymentsServices
+
+- (instancetype)init
+{
+    HSBCPaymentsTenantMap *defaultTenant = [HSBCPaymentsTenantMap tenantWithName:@"DEFAULT"];
+    
+    HyperTenantParams *tenantParams = [[HyperTenantParams alloc] init];
+    tenantParams.tenantId = defaultTenant.tenantId;
+    tenantParams.releaseConfigURL = defaultTenant.releaseConfigTemplateUrl;
+    
+    self = [super initWithTenantParams:tenantParams];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (instancetype)initWithClientId:(NSString *)clientId {
+    return [self initWithTenantId:@"DEFAULT" clientId:clientId];
+}
+
+- (instancetype)initWithTenantId:(NSString *)tenantId clientId:(NSString *)clientId {
+    HSBCPaymentsTenantMap *tenant = [HSBCPaymentsTenantMap tenantWithName:tenantId];
+    
+    if (!tenant) {
+        NSLog(@"Tenant '%@' not found, falling back to DEFAULT", tenantId);
+        tenant = [HSBCPaymentsTenantMap tenantWithName:@"DEFAULT"];
+    }
+    
+    HyperTenantParams *tenantParams = [[HyperTenantParams alloc] init];
+    tenantParams.clientId = clientId;
+    tenantParams.tenantId = tenant.tenantId;
+    tenantParams.releaseConfigURL = tenant.releaseConfigTemplateUrl;
+    
+    self = [super initWithTenantParams:tenantParams];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)initiate:(UIViewController *)viewController payload:(NSDictionary *)initiationPayload callback:(HSBCPaymentsCallback)callback {
+    self.tenantServiceName = initiationPayload[@"service"];
+    NSDictionary *updatedPayload = [HSBCPaymentsUtils updateServiceNameInPayload:initiationPayload tenantServiceName:self.tenantServiceName isResponse:false];
+    __weak HSBCPaymentsServices *weakSelf = self;
+    [super initiate:viewController payload:updatedPayload callback:^(NSDictionary<NSString *,id> * _Nullable data) {
+        NSDictionary *updatedResponse = [HSBCPaymentsUtils updateServiceNameInPayload:data tenantServiceName:weakSelf.tenantServiceName isResponse:true];
+        callback(updatedResponse);
+    }];
+}
+
+- (void)process:(UIViewController *)viewController processPayload:(NSDictionary *)processPayload {
+    NSDictionary *updatedPayload = [HSBCPaymentsUtils updateServiceNameInPayload:processPayload tenantServiceName:self.tenantServiceName isResponse:false];
+    [super process:viewController processPayload:updatedPayload];
+}
+
+- (void)process:(NSDictionary *)processPayload {
+    NSDictionary *updatedPayload = [HSBCPaymentsUtils updateServiceNameInPayload:processPayload tenantServiceName:self.tenantServiceName isResponse:false];
+    [super process:updatedPayload];
+}
+
+- (HSBCPaymentsEventsCallback)merchantEvent {
+    return [super merchantEvent];
+}
+
+- (void)setDelegate:(id<HSBCPaymentsDelegate>)delegate {
+    [super setHyperDelegate:delegate];
+    _delegate = delegate;
+}
+
+@end
